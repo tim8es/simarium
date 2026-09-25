c99cf441cd3491ae29c864428f11f1ec8e81043c
tim8es <108188608+tim8es@users.noreply.github.com>
2026-09-25 17:15:55 +0300

    feat: export phase-4 detritivore API

--- packages/sim-core/src/index.ts
+++ packages/sim-core/src/index.ts
@@ -4,6 +4,7 @@ export * from "./ledger.js";
 export * from "./material.js";
 export * from "./phase2.js";
 export * from "./phase3.js";
+export * from "./phase4.js";
 export * from "./rng.js";
 export * from "./scheduler.js";
 export * from "./serialization.js";
