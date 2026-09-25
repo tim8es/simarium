8405141bb56af5897ecbf1110247b9491cc750d1
tim8es <108188608+tim8es@users.noreply.github.com>
2026-09-25 17:23:41 +0300

    feat: export phase-6 Dalotia API

--- packages/sim-core/src/index.ts
+++ packages/sim-core/src/index.ts
@@ -6,6 +6,7 @@ export * from "./phase2.js";
 export * from "./phase3.js";
 export * from "./phase4.js";
 export * from "./phase5.js";
+export * from "./phase6.js";
 export * from "./rng.js";
 export * from "./scheduler.js";
 export * from "./serialization.js";
